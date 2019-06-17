b8f1ea26a6ee92b41aec07d5cf7494c08fba04ba
Yuning Shen <ynshen23@gmail.com>
2019-06-17 10:44:47 -0700

    update package name

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='pkg_private_test',
     version='0.0.1',
-    description='My private package from private github repo',
+    description='The test private package from private github repo',
     url='git@github.com:ynshen/pkg_private_test.git',
     author='Yuning Shen',
     author_email='ynshen23@gmail.com',
     license='unlicense',
-    packages=['pkg_private_test'],
+    packages=['pkg_private'],
     zip_safe=False
 )
