b763a981f119159c70352446f89aad0c185cd449
Yuning Shen <ynshen23@gmail.com>
2019-06-17 10:41:58 -0700

    update git url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pkg_private_test',
     version='0.0.1',
     description='My private package from private github repo',
-    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    url='git@github.com:ynshen/pkg_private_test.git',
     author='Yuning Shen',
     author_email='ynshen23@gmail.com',
     license='unlicense',
