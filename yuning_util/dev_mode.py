4706a9cc656c28adde868b65d5df13995d2b058e
Yuning Shen <ynshen23@gmail.com>
2019-12-12 11:04:38 -0800

    add logger redirect

--- yuning_util/dev_mode.py
+++ yuning_util/dev_mode.py
@@ -5,7 +5,8 @@ class DevMode:
         from pathlib import Path
         
         pkgs = {
-            'k-seq': '/home/yuning/research/k-seq/src/pkg/'
+            'k-seq': '/home/yuning/research/k-seq/src/',
+            'skin-mb': '/home/yuning/research/skin_microbiome/src/'
         }
         
         if pkg in pkgs.keys():
@@ -17,13 +18,24 @@ class DevMode:
         if auto_on:
             self.on()
     
-    def on(self):
+    def on(self, get_log_handler=False):
+        """Turn on the dev mode"""
         import sys
+        
+        # prioritize the dev package location
         if self.pkg_path not in sys.path:
             sys.path.insert(0, self.pkg_path)
         elif sys.path.index(self.pkg_path) !=0:
             sys.path.remove(self.pkg_path)
             sys.path.insert(0, self.pkg_path)
+
+        # redirect logging info to standard output
+        import logging
+        root = logging.getLogger()
+        handler = logging.StreamHandler(sys.stdout)
+        root.addHandler(handler)
+        if get_log_handler:
+            return handler
     
     def off(self):
         if self.pkg_path in sys.path:
