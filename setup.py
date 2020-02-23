41e4c3a0b94e363a1d6e7ea77bf7d10393efbedd
Yuning Shen <ynshen23@gmail.com>
2020-02-22 16:42:46 -0800

    update setup.py; bump up to 0.5.0

--- setup.py
+++ setup.py
@@ -1,13 +1,36 @@
-import setuptools
+"""Setup for yutility
+See: https://github.com/ynshen/yutility
+"""
 
-setuptools.setup(
-    name='yuning-util',
-    version='0.4',
-    description='Daily utility functions in develop',
-    url='git@github.com:ynshen/utility.git',
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='yutility',
+    version='0.5.0',
+    description='Utility functions for development',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ynshen/yutility/',
     author='Yuning Shen',
     author_email='ynshen23@gmail.com',
-    license='unlicense',
-    packages=setuptools.find_packages(),
-    zip_safe=False
+    keywords='development utility',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.5',
+    install_requires=[],
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/ynshen/yutility/issues/',
+        'Source': 'https://github.com/ynshen/yutility/',
+    },
 )
