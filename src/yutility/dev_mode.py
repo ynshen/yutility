a78d6916ec77f61d8d64422d173709d5d527da3e
Yuning Shen <ynshen23@gmail.com>
2020-03-04 22:43:24 -0800

    add support for ENV variables

--- src/yutility/dev_mode.py
+++ src/yutility/dev_mode.py
@@ -1,118 +1,160 @@
-"""Quick setup for my local/tower develop environemnt
+"""Quick setup for my local/tower develop environment
 """
 import sys
 import json
 from pathlib import Path
-import logging
+from .log import logging
 import os
 
 
+def on(project):
+    dev_mode = DevMode()
+    dev_mode.on(project)
+
+
+def off(project):
+    dev_mode = DevMode()
+    dev_mode.off(project)
+
+
 class DevMode:
-    """Control package path in the development environment. Load info from
-        ``~/.yutil.config`` if file exists
+    """Control package path in the development environment for projects, include pythonpath and ENV variables
+
+    Can load and save info from
+        ``~/.yutil.config``, with format
+        {project: {pkg_path: [], env_var: {name: value}}
     """
 
     def __repr__(self):
 
         def proj_formatter(proj):
-            return f"{proj}:\n     " + '\n    '.join(self.pkg_path[proj])
-        return '\n'.join([proj_formatter(proj) for proj in self.pkg_path.keys()])
+            f = proj + ':\n'
+            f += '  Package:\n    '
+            f += '\n    '.join(self.config[proj]['pkg_path'])
+            f += '\n  ENV variable:\n    '
+            f += '\n    '.join([f'{env}: {var}' for env, var in self.config[proj]['env_var'].items()])
+            return f
+        return '\n'.join([proj_formatter(proj) for proj in self.config.keys()])
 
     def __init__(self, project=None, auto_on=True):
-        """Start a DevMode with a project
-        Projects:
-          - k-seq
-          - skin-mb
-          - microNet
-       """
+        """Initialize a DevMode controller"""
 
         _HOME = os.getenv('HOME', '~')
         if Path(_HOME + '/.yutil.config').exists():
             with open(_HOME + '/.yutil.config', 'r') as handle:
-                _config_info = json.load(handle)
+                self.config = json.load(handle)
         else:
-            _config_info = dict(pkg_path=dict())
+            self.config = dict()
 
-        self.pkg_path = _config_info['pkg_path']
         self.current_on = None
 
         if auto_on and project is not None:
             self.on(project)
 
     def add_project(self, project):
-        """Add a project"""
-        if project not in self.pkg_path.keys():
-            self.pkg_path[project] = []
+        """Add a project, if not exist"""
+        if project not in self.config.keys():
+            self.config[project] = dict(pkg_path=[], env_var={})
         else:
-            logging.error(f"Project '{project}' exists")
-            raise ValueError(f"Project '{project}' exists")
+            logging.error(f"Project '{project}' exists", error_type=ValueError)
 
     def add_pkg_path(self, path, project=None):
         """Add pkg path under a project name"""
+
         if Path(path).exists():
             if project is None:
-                if self.pkg_path == {}:
-                    logging.error('yutil.config seems empty, indicate the project name')
-                    raise ValueError('yutil.config seems empty, indicate the project name')
+                if self.config == {}:
+                    logging.error('Configuration seems empty, please add project first', error_type=ValueError)
                 else:
-                    for pkg_list in self.pkg_path.values():
-                        pkg_list.append(path)
+                    for p in self.config.values():
+                        if path not in p['pkg_path']:
+                            p['pkg_path'].append(path)
             else:
-                if project in self.pkg_path.keys():
-                    self.pkg_path[project].append(path)
-                else:
-                    self.pkg_path[project] = [path]
+                if project not in self.config.keys():
+                    self.add_project(project=project)
+                if path not in self.config[project]['pkg_path']:
+                    self.config[project]['pkg_path'].append(path)
         else:
-            logging.error(f'{path} does not seem to be an existing directory')
-            raise NotADirectoryError(f'{path} does not seem to be an existing directory')
+            logging.error(f'{path} does not seem to be an existing directory', error_type=NotADirectoryError)
+
+    def add_env_var(self, project=None, **kwargs):
+        """Add environmental variables [to a project]
+        Warning: old environmental variable could be overwrote
+        """
+
+        if project is None:
+            if self.config == {}:
+                logging.error('Configuration seems empty, please add project first', error_type=ValueError)
+            else:
+                for p in self.config.values():
+                    p['env_var'].update(kwargs)
+        else:
+            if project not in self.config.keys():
+                self.add_project(project=project)
+            self.config[project]['env_var'].update(kwargs)
+
+    def delete_project(self, project):
+        _ = self.config.pop(project)
 
     def delete_pkg_path(self, path, project=None):
         """Delete path from given project"""
         if project is None:
-            for pkg_list in self.pkg_path.values():
+            for pkg_list in self.config['pkg_path'].values():
                 _ = pkg_list.pop(path)
         else:
-            if project not in self.pkg_path.keys():
-                logging.error(f"'{project}' not found")
-                raise ValueError(f"'{project}' not found")
+            if project not in self.config['pkg_path'].keys():
+                logging.error(f"'{project}' not found", error_type=ValueError)
             else:
-                self.pkg_path[project].pop(path)
+                self.config[project]['pkg_path'].pop(path)
+
+    def delete_env_var(self, env_var, project=None):
+        """Delete env var from given project"""
+        if project is None:
+            for pkg_list in self.config['env_var'].values():
+                _ = pkg_list.pop(env_var)
+        else:
+            if project not in self.config['pkg_path'].keys():
+                logging.error(f"'{project}' not found", error_type=ValueError)
+            else:
+                _ = self.config[project]['pkg_path'].pop(env_var)
 
     def on(self, project):
-        """Turn on the dev mode by prioritize python PATHs"""
+        """Turn on the dev mode by prioritizing python PATHs and assigning env variable values"""
 
-        if project not in self.pkg_path.keys():
-            logging.error(f'{project} not found')
-            raise ValueError(f'{project} not found')
+        if project not in self.config.keys():
+            logging.error(f'{project} not found', error_type=ValueError)
 
         # prioritize the dev package location
-        for p in self.pkg_path[project][::-1]:
+        for p in self.config[project]['pkg_path'][::-1]:
             if p not in sys.path:
                 sys.path.insert(0, p)
             elif sys.path.index(p) != 0:
                 sys.path.remove(p)
                 sys.path.insert(0, p)
 
+        for env_var, value in self.config[project]['env_var'].items():
+            os.environ[env_var] = value
+
         # redirect logging info to standard output
-        root = logging.getLogger()
-        if root.handlers != [] and any(
-            [handler.name is not None and 'stdout' in handler.name
-             for handler in root.handlers]
-        ):
-            pass
-        else:
-            handler = logging.StreamHandler(sys.stdout)
-            handler.name = 'stdout'
-            root.addHandler(handler)
-        root.setLevel(logging.INFO)
+        logging.add_console_handler()
         self.current_on = project
 
-    def off(self):
-        for p in self.pkg_path:
+    def off(self, project=None):
+        if project is None:
+            project = self.current_on
+        if project is None:
+            logging.error('DevMode is not on', error_type=ValueError)
+
+        for p in self.config[project]['pkg_path']:
             if p in sys.path:
                 sys.path.remove(p)
+
+        for env in self.config[project]['env_var'].keys():
+            del os.environ[env]
+
+        self.current_on = None
 
     def save(self):
         _HOME = os.getenv('HOME', '~')
         with open(_HOME + '/.yutil.config', 'w') as handle:
-            json.dump(obj={'pkg_path': self.pkg_path}, fp=handle)
+            json.dump(obj=self.config, fp=handle)
--- test/dev_mode_test.py
+++ test/dev_mode_test.py
@@ -10,7 +10,7 @@ def test_DevMode_can_create():
 
 def test_DevMode_can_add_path():
     dev_mode = DevMode()
-    if 'project1' not in dev_mode.pkg_path.keys():
+    if 'project1' not in dev_mode.config.keys():
         dev_mode.add_project('project1')
     with raises(NotADirectoryError):
         dev_mode.add_pkg_path(path='some/fake/path', project='project2')
@@ -18,27 +18,39 @@ def test_DevMode_can_add_path():
     import os
     _HOME = os.getenv('HOME')
     dev_mode.add_pkg_path(path=_HOME, project='project2')
-    assert 'project2' in dev_mode.pkg_path.keys()
-    assert dev_mode.pkg_path['project2'] == [_HOME]
+    assert 'project2' in dev_mode.config.keys()
+    assert dev_mode.config['project2']['pkg_path'] == [_HOME]
+
+
+def test_DevMode_can_add_env_var():
+    dev_mode = DevMode()
+    if 'project1' not in dev_mode.config.keys():
+        dev_mode.add_project('project1')
+    dev_mode.add_env_var(FAKE_ENV='value', project='project2')
+    import os
+    assert os.getenv('FAKE_ENV', None) is None
+    dev_mode.on('project2')
+    assert os.getenv('FAKE_ENV', None) == 'value'
 
 
 def test_DevMode_can_switch_on_and_off():
     dev_mode = DevMode()
-    if 'project1' not in dev_mode.pkg_path.keys():
+    if 'project1' not in dev_mode.config.keys():
         dev_mode.add_project('project1')
     with raises(NotADirectoryError):
         dev_mode.add_pkg_path(path='some/fake/path', project='project2')
     dev_mode.on('project1')
     assert dev_mode.current_on == 'project1'
     dev_mode.off()
+    assert dev_mode.current_on is None
 
 
-def test_DevMode_can_save():
-    dev_mode = DevMode()
-    if 'project1' not in dev_mode.pkg_path.keys():
-        dev_mode.add_project('project1')
-    with raises(NotADirectoryError):
-        dev_mode.add_pkg_path(path='some/fake/path', project='project2')
-    dev_mode.on('project1')
-
-    dev_mode.save()
+# def test_DevMode_can_save():
+#     dev_mode = DevMode()
+#     if 'project1' not in dev_mode.config.keys():
+#         dev_mode.add_project('project1')
+#     with raises(NotADirectoryError):
+#         dev_mode.add_pkg_path(path='some/fake/path', project='project2')
+#     dev_mode.on('project1')
+#
+#     dev_mode.save()
